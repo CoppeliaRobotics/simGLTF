d38a7bf5e09b52f983a7164bc70ddc5012c71df6
Federico Ferri <federico.ferri.it@gmail.com>
2020-01-16 18:40:26 +0100

    improve add-on

--- simAddOnFunc_gltfExporter.lua
+++ simAddOnFunc_gltfExporter.lua
@@ -1,12 +1,14 @@
-local appPath=sim.getStringParameter(sim.stringparam_application_path)
-local fileName=sim.fileDialog(sim.filedlg_type_save,'Export to glTF...',appPath,'scene.gltf','glTF file','gltf')
-local includeAnimation=sim.msgBox(sim.msgbox_type_question,sim.msgbox_buttons_yesno,'Export to glTF...','Include also animation content (from last simulation run)?')==sim.msgbox_return_yes
-if fileName~='' then
-    m=simGLTF.create()
-    simGLTF.exportAllObjects(m)
-    if includeAnimation then
-        simGLTF.exportAnimation(m)
-    end
-    simGLTF.saveASCII(m,fileName)
-    simGLTF.destroy(m)
+local scenePath=sim.getStringParameter(sim.stringparam_scene_path)
+local sceneName=sim.getStringParameter(sim.stringparam_scene_name):match("(.+)%..+")
+if sceneName==nil then sceneName='untitled' end
+local fileName=sim.fileDialog(sim.filedlg_type_save,'Export to glTF...',scenePath,sceneName..'.gltf','glTF file','gltf')
+if fileName==nil then return end
+local includeAnimation=simGLTF.animationFrameCount()>0 and sim.msgBox(sim.msgbox_type_question,sim.msgbox_buttons_yesno,'Export to glTF...','Include also animation content (from last simulation run)?')==sim.msgbox_return_yes
+m=simGLTF.create()
+simGLTF.exportAllObjects(m)
+if includeAnimation then
+    simGLTF.exportAnimation(m)
 end
+simGLTF.saveASCII(m,fileName)
+sim.addStatusbarMessage('Exported glTF content to '..fileName)
+simGLTF.destroy(m)
