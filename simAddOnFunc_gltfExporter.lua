7a34ffcca3627a7bcb9348c99290d30c20ef8643
Federico Ferri <federico.ferri.it@gmail.com>
2020-02-04 10:32:46 +0100

    move animation export to its own add-on script

--- simAddOnFunc_gltfExporter.lua
+++ simAddOnFunc_gltfExporter.lua
@@ -3,12 +3,7 @@ local sceneName=sim.getStringParameter(sim.stringparam_scene_name):match("(.+)%.
 if sceneName==nil then sceneName='untitled' end
 local fileName=sim.fileDialog(sim.filedlg_type_save,'Export to glTF...',scenePath,sceneName..'.gltf','glTF file','gltf')
 if fileName==nil then return end
-local exportAnimation=simGLTF.animationFrameCount()>0 and sim.msgBox(sim.msgbox_type_question,sim.msgbox_buttons_yesno,'Export to glTF...','Export animation content from last simulation run?')==sim.msgbox_return_yes
-if exportAnimation then
-    simGLTF.exportAnimation()
-else
-    simGLTF.clear()
-    simGLTF.exportAllObjects()
-end
+simGLTF.clear()
+simGLTF.exportAllObjects()
 simGLTF.saveASCII(fileName)
 sim.addStatusbarMessage('Exported glTF content to '..fileName)
